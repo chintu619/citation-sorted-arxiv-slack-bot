dc0c911cd4af4cd8aa341a435a931777e4835ced
Chaitanya Narisetty <c-narisetty@cp.jp.nec.com>
2019-09-25 09:58:13 +0900

    Bugs & fixes.

--- arXiv_bot.py
+++ arXiv_bot.py
@@ -9,6 +9,7 @@ import os,requests,pandas,json,feedparser
 import scholarly
 import numpy as np
 import time
+import datetime as dt
 
 def getAuthorCitations(author):
 #    print(author)
@@ -43,7 +44,7 @@ def search(posted_ids,Texts,verify):
     while True:
         counter = 0
         arXiv_url = 'http://export.arxiv.org/api/query?search_query='
-        query='(cat:stat.ML+OR+cat:cs.CV+OR+cat:cs.LG)&start=0&max_results=300' + \
+        query='(cat:stat.ML+OR+cat:cs.CV+OR+cat:cs.LG)&start=0&max_results=1000' + \
               '&sortBy=lastUpdatedDate&sortOrder=descending'
         arXiv_url = arXiv_url+query
         data = requests.get(arXiv_url,verify=verify).text
@@ -55,12 +56,17 @@ def search(posted_ids,Texts,verify):
         for val in entry_elements:
             paper_info = {}
             paper_info['url']= val['id']
-            paper_info['title'] = val['title']
-            paper_info['abstract'] = val['summary']
-            paper_info['date'] = val['published']
+            paper_info['title'] = ''.join(val['title'].split('\n '))
+            paper_info['abstract'] = ' '.join(val['summary'].split('\n'))
+            paper_info['release_date'] = val['published']
+            paper_info['date'] = dt.date.today().strftime('%Y-%m-%d')
 
             if (paper_info['url'] not in posted_urls) and (paper_info['url'][-2:] == 'v1'):
+
                 if not flag_IPban:
+                    # sleep for 15 minutes after checking of every 50 papers to avoid IP ban
+                    if (counter > 0) and (counter%50 == 0): time.sleep(15*60)
+
                     citations = 0 # total citation count of the paper
                     # get a list of all the authors in the paper
                     author_list = [author['name'] for author in val['authors']]
@@ -75,43 +81,57 @@ def search(posted_ids,Texts,verify):
                             flag_IPban = True
                             break
 
-                pub_date = paper_info['date'].split('T')[0]
-                title = ''.join(paper_info['title'].split('\n '))
-                all_paper_info.append([paper_info['url'],pub_date,citations,title])
                 paper_citation_count.append(citations)
                 counter += 1; print('Paper:', counter, 'Citations:', citations)
+
                 if citations is np.nan:
-                    all_texts.append(('Title:*UNRANKED! - {title}*\nDate:{date}\nAbstract' + \
+                    all_texts.append(('Title:*UNRANKED! - {title}*\nDate:{release_date}\nAbstract' + \
                                     ':```{abstract}```\n{url}').format(**paper_info))
+                    title_onset = 'UNRANKED! - '
                 else:
-                    all_texts.append(('Title:*{title}*\nDate:{date}\nAbstract' + \
+                    all_texts.append(('Title:*{title}*\nDate:{release_date}\nAbstract' + \
                                     ':```{abstract}```\n{url}').format(**paper_info))
+                    title_onset = ''
+
+                release_date = paper_info['release_date'].split('T')[0]
+                title = title_onset + paper_info['title']
+                all_paper_info.append([paper_info['url'],paper_info['date'],citations,title,release_date])
 
         # rank the papers according to their authors' citation counts
         rankings = np.argsort(paper_citation_count)[::-1]
         all_texts_sorted = [all_texts[rank] for rank in rankings]
-        all_paper_info = pandas.DataFrame(all_paper_info, columns=['url','date','weight','title'])
+        all_paper_info = pandas.DataFrame(all_paper_info, columns=['url','date','weight','title', 'release_date'])
         posted_ids = pandas.concat((all_paper_info, posted_ids), ignore_index=True)
         Texts.extend(all_texts_sorted)
 
         return posted_ids, Texts
 
 def post(Texts,Web_hook_url,verify):
+    post_text = ''; counter = 1
     for val in Texts:
-        post = {'text':val,
-                'username':u'Bot',
-                'icon_emoji':u':thinking_face:',
-                'unfurl_links':True,
-                'link_names':1,}
-        data = json.dumps(post).encode("utf-8")
-        requests.post(Web_hook_url, data = data,verify=verify)
+        # only use title and url information
+        title = val.split('\n')[0][7:-1] # remove first 6 letters (title:*) and last letter (*)
+        url = 'http://arxiv.org/abs/' + val.split('http://arxiv.org/abs/')[-1].split('\n')[0]
+        post_text += str(counter) + '. ' + title + ' - ' + url + '\n'
+        counter += 1
+    post = {'text':post_text,
+            'username':u'Bot',
+            'icon_emoji':u':thinking_face:',
+            'unfurl_links':False,
+            'link_names':1,
+            'channel': '#misc-news-arxiv',}
+    data = json.dumps(post).encode("utf-8")
+    requests.post(Web_hook_url, data = data,verify=verify)
 
 def makeMarkdown(posted_ids):
-    yesterdate = (dt.date.today()- dt.timedelta(1)).strftime('%Y-%m-%d')
-    yesterday_papers = posted_ids.loc[posted_ids['date'] == yesterdate].reset_index(drop=1)
+    if len(posted_ids) == 0: return 0
 
-    oneweekpastdate = (dt.date.today()- dt.timedelta(1+7)).strftime('%Y-%m-%d')
-    lastweek_papers = posted_ids.loc[(posted_ids['date'] <= yesterdate) & \
+    lastRelease_date = max(posted_ids.date)
+    lastRelease_dt = dt.datetime.strptime(lastRelease_date, '%Y-%m-%d')
+    lastRelease_papers = posted_ids.loc[posted_ids['date'] == lastRelease_date].reset_index(drop=1)
+
+    oneweekpastdate = (lastRelease_dt - dt.timedelta(7)).strftime('%Y-%m-%d')
+    lastweek_papers = posted_ids.loc[(posted_ids['date'] <= lastRelease_date) & \
                                      (posted_ids['date'] > oneweekpastdate)]
     lastweek_papers = lastweek_papers.sort_values(by='weight', ascending=False \
                                                  ).reset_index(drop=1)
@@ -124,8 +144,8 @@ def makeMarkdown(posted_ids):
     after_weekly = '</details>\n'.join(after_weekly[1:])
 
     markdown = before_daily + "## Daily top 10\n"
-    for itr in range(len(yesterday_papers)):
-        paper = yesterday_papers.loc[itr]
+    for itr in range(len(lastRelease_papers)):
+        paper = lastRelease_papers.loc[itr]
         if itr == 10:
             markdown += "<details><summary>today's remaining papers</summary>\n" + \
                         "  <ol start=11>\n"
@@ -135,7 +155,7 @@ def makeMarkdown(posted_ids):
             markdown += '    <li><i>'+paper['title']+'</i> <a href="'+\
                                       paper['url']+'">url</a></li>\n'
 
-    if len(yesterday_papers) > 10: markdown += "  </ol>\n</details>\n\n" + "## Weekly top 20\n"
+    if len(lastRelease_papers) > 10: markdown += "  </ol>\n</details>\n\n" + "## Weekly top 20\n"
     else: markdown += "<details><summary>today's remaining papers</summary>\n" + \
                         "  <ol start=11>\n" + "  </ol>\n</details>\n\n" + "## Weekly top 20\n"
 
@@ -163,7 +183,7 @@ if __name__== '__main__':
     if os.path.exists(posted_ids_file):
         posted_ids = pandas.read_csv(posted_ids_file)
     else:
-        posted_ids = pandas.DataFrame([], columns=['url', 'date', 'weight', 'title'])
+        posted_ids = pandas.DataFrame([], columns=['url', 'date', 'weight', 'title', 'release_date'])
 
     Texts = []
     posted_ids, Texts = search(posted_ids,Texts,verify)
@@ -174,3 +194,12 @@ if __name__== '__main__':
     posted_ids = posted_ids.sort_values(by=['date','weight'], ascending=False).reset_index(drop=1)
     posted_ids.to_csv(posted_ids_file, index=False)
     makeMarkdown(posted_ids)
+
+    # # auto-update git repo
+    # try:
+    #     if os.popen('git diff README.md posted_ids.csv').read():
+    #         print(os.popen('git add README.md posted_ids.csv').read())
+    #         #print(os.popen('git status').read())
+    #         print(os.popen('git commit -m "Updated to today\'s feed."').read())
+    #         print(os.popen('git push').read())
+    # except: print('Please check git master status.')
